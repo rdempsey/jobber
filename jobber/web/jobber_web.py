8c6497c37ca584855ec210319f4f63619e2554d8
Robert Dempsey <robertonrails@gmail.com>
2017-04-08 23:50:43 -0400

    Added ability to apply for a job
    Oh yeah!

--- jobber/web/jobber_web.py
+++ jobber/web/jobber_web.py
@@ -6,6 +6,7 @@ from flask import Flask, abort, flash, redirect, render_template, request, url_f
 import traceback
 from momentjs import momentjs
 import uuid
+from collections import defaultdict
 
 app = Flask(__name__)
 app.jinja_env.globals['momentjs'] = momentjs
@@ -15,6 +16,8 @@ app.config['SECRET_KEY'] = 'some_really_long_random_string_here'
 app_port = int(getenv('APP_PORT', '5000'))
 questions_endpoint = getenv('QUESTIONS_ENDPOINT', 'http://localhost:8080/1.0/questions')
 application_endpoint = getenv('APPLICATION_ENDPOINT', 'http://localhost:8080/1.0/job-applications')
+
+nested_dict = lambda: defaultdict(nested_dict)
 
 
 @app.route('/')
@@ -33,7 +36,6 @@ def show_job_applications():
         # Datetime format: 2017-04-08 18:33:43.303353
         job_application['created_at'] = datetime.strptime(job_application['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
         job_application['updated_at'] = datetime.strptime(job_application['updated_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        logging.info(job_application)
         job_applications.append(job_application)
 
     sorted_applications = sorted(job_applications, key=lambda k: k['created_at'], reverse=True)
@@ -54,6 +56,44 @@ def show_job_application(job_application_id):
     return render_template('job_applications/show.html', job_application=job_application)
 
 
+@app.route('/apply', methods = ['GET'])
+def apply_for_job():
+    questions = list()
+
+    r = requests.get(questions_endpoint)
+    response = r.json()
+
+    for question in response['data']['questions']:
+        del question['answer']
+        del question['created_at']
+        del question['updated_at']
+        questions.append(question)
+
+    return render_template('/job_applications/new.html', questions=questions)
+
+
+@app.route('/save-job-application', methods = ['POST'])
+def save_job_application():
+    aid = uuid.uuid5(uuid.NAMESPACE_DNS, 'jobber')      # application name
+    aid = uuid.uuid5(aid, request.form['applicant_name'])         # applicant name
+    aid = uuid.uuid5(aid, str(datetime.utcnow()))            # current_time
+
+    job_application = nested_dict()
+
+    job_application['id'] = str(aid)
+    job_application['name'] = request.form['applicant_name']
+    job_application['applicant_responses'] = list()
+
+    for qid, answer in request.form.to_dict().items():
+        if qid != 'applicant_name':
+            job_application['applicant_responses'].append({"answer": answer, "id": qid})
+
+    url = "{}/{}".format(application_endpoint, aid)
+    requests.put(url, json=job_application)
+
+    return redirect('job-applications')
+
+
 @app.route('/questions', methods = ['GET', 'POST'])
 def show_questions():
 
@@ -69,7 +109,6 @@ def show_questions():
         }
         url = "{}/{}".format(questions_endpoint, question_data['id'])
         requests.put(url, json=question_data)
-
 
     questions = list()
 
